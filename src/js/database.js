3b850bb61139d80ce0d10cf0e9e9c9d33657e4bf
Thierry Thuon <lepiaf@users.noreply.github.com>
2015-02-21 19:31:35 +0100

    place all point

--- src/js/database.js
+++ src/js/database.js
@@ -2,55 +2,183 @@ var database = {
     photos: [
         {
             name: "0001",
-            camera: {alpha: 0,beta: Math.PI/2},
+            camera: {
+                alpha: 0,
+                beta: Math.PI/2
+            },
             neighbors: [
-                {name:"0002",cursor:{x:-6,y:-5,z:0}}
+                {
+                    name: "0002",
+                    cursor: {
+                        x: -9,
+                        y: -6,
+                        z: 0,
+                        rotate: {
+                            x: -.2,
+                            y: 0,
+                            z: -(Math.PI/2)-0.1
+                        }
+                    }
+                }
             ]
         },
         {
             name: "0002",
-            camera: {alpha: 0,beta: Math.PI/2},
+            camera: {
+                alpha: 0,
+                beta: Math.PI/2
+            },
             neighbors: [
-                {name:"0001",cursor:{x:6,y:5,z:0}},
-                {name:"0003",cursor:{x:0,y:-5,z:5}}
+                {
+                    name: "0001",
+                    cursor: {
+                        x: 12,
+                        y: -5,
+                        z: 0,
+                        rotate: {
+                            x: 0,
+                            y: 0,
+                            z: (Math.PI/2)+.1
+                        }
+                    }
+                },
+                {
+                    name: "0003",
+                    cursor: {
+                        x: 1,
+                        y: -5,
+                        z: 7,
+                        rotate: {
+                            x: -(Math.PI/2),
+                            y: 0,
+                            z: 0
+                        }
+                    }
+                }
             ]
         },
         {
             name: "0003",
-            camera: {alpha: 0,beta: Math.PI/2},
+            camera: {
+                alpha: (Math.PI/2),
+                beta: Math.PI/2
+            },
             neighbors: [
-                {name:"0002",cursor:{x:0,y:0,z:5}},
-                {name:"0004",cursor:{x:0,y:0,z:5}}
+                {
+                    name: "0002",
+                    cursor: {
+                        x: 0,
+                        y: -3,
+                        z: 9,
+                        rotate: {
+                            x: -(Math.PI/2),
+                            y: 0,
+                            z: .1
+                        }
+                    }
+                },
+                {
+                    name: "0004",
+                    cursor: {
+                        x: 10,
+                        y: -3,
+                        z: 0,
+                        rotate: {
+                            x: -(Math.PI/2),
+                            y: 0,
+                            z: (Math.PI/2)
+                        }
+                    }
+                }
             ]
         },
         {
             name: "0004",
-            camera: {alpha: 0,beta: Math.PI/2},
+            camera: {
+                alpha: Math.PI,
+                beta: Math.PI/2
+            },
             neighbors: [
-                {name:"0003",cursor:{x:0,y:0,z:5}},
-                {name:"0005",cursor:{x:0,y:0,z:5}}
+                {
+                    name: "0003",
+                    cursor: {
+                        x: -10,
+                        y: -5,
+                        z: 5,
+                        rotate: {
+                            x: 0, y: (Math.PI/2)-1, z: -(Math.PI/2)
+                        }
+                    }
+                },
+                {
+                    name: "0005",
+                    cursor: {
+                        x: -2,
+                        y: -5,
+                        z: 10,
+                        rotate: {
+                            x: -(Math.PI/2), y: 0, z: 0
+                        }
+                    }
+                }
             ]
         },
         {
             name: "0005",
-            camera: {alpha: 0,beta: Math.PI/2},
+            camera: {
+                alpha: 0,
+                beta: Math.PI/2
+            },
             neighbors: [
-                {name:"0004",cursor:{x:0,y:0,z:5}},
-                {name:"0006",cursor:{x:0,y:0,z:5}}
+                {
+                    name: "0006",
+                    cursor: {
+                        x: -10,
+                        y: -5,
+                        z: 5,
+                        rotate: {
+                            x: 0, y: (Math.PI/2)-1.5, z: -(Math.PI/2)
+                        }
+                    }
+                },
+                {
+                    name: "0004",
+                    cursor: {
+                        x: -2,
+                        y: -5,
+                        z: 10,
+                        rotate: {
+                            x: -(Math.PI/2), y: 0, z: 0
+                        }
+                    }
+                }
             ]
         },
         {
             name: "0006",
-            camera: {alpha: 0,beta: Math.PI/2},
+            camera: {
+                alpha: -(Math.PI/2),
+                beta: Math.PI/2
+            },
             neighbors: [
-                {name:"0005",cursor:{x:0,y:0,z:5}}
+                {
+                    name: "0005",
+                    cursor: {
+                        x: 7,
+                        y: -5,
+                        z: -15,
+                        rotate: {
+                            x: (Math.PI/2), y: -0.2, z: 0
+                        }
+                    }
+                }
             ]
         }
     ]
 };
 
 var findPhoto = function(name){
-    var photo = "";
+    var photo = {};
     $.each(database.photos, function(i, e){
         if (e.name === name) {
             photo = e;
--- src/js/photosphere.js
+++ src/js/photosphere.js
@@ -1,8 +1,105 @@
-(function() {
-    var app = angular.module('photosphereApp', []);
-    
-    app.controller('PhotosphereController',function(){
-        this.global = {};
-        this.canvas = "";
+var global = {};
+
+// setups normal camera
+var setupNormalCamera = function(camera) {
+    // This creates and positions the arc camera
+    global.camera = new BABYLON.ArcRotateCamera("ArcRotateCamera", camera.alpha, camera.beta, 15, new BABYLON.Vector3(0, 0, 0), global.scene);
+
+    // limiting the upper and lower bounds removes the scrollwheel behaviour
+    global.camera.lowerRadiusLimit = global.camera.radius;
+    global.camera.upperRadiusLimit = global.camera.radius;
+
+    // This attaches the camera to the canvas
+    global.camera.attachControl(global.canvas, false);
+};
+
+// setup photosphere
+var setupPhotoSphere = function(current, database){
+
+    // Our built-in 'sphere' shape. Params: name, subdivs, size, scene
+    var sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 100, global.scene);
+
+    if (typeof global.moveblock != "undefined" && global.moveblock.length > 0) {
+        $.each(global.moveblock, function (i, e) {
+
+            e.dispose();
+        });
+    }
+
+    if(global.scene.materials.length > 0) {
+      $.each(global.scene.materials, function (i,e){
+        global.scene.materials.splice(i,1);
+      });
+    }
+
+    global.moveblock = [];
+    var photo = findPhoto(current);
+    console.log(current);
+    $.each(photo.neighbors, function (i, e){
+        var moveBlock = BABYLON.Mesh.CreateCylinder("move_"+ e.name, 5, 1, 0, 12, 1, global.scene, true);
+        moveBlock.position = new BABYLON.Vector3(e.cursor.x,e.cursor.y,e.cursor.z);
+        moveBlock.rotation.x = e.cursor.rotate.x;
+        moveBlock.rotation.y = e.cursor.rotate.y;
+        moveBlock.rotation.z = e.cursor.rotate.z;
+
+        var materialMoveBlock = new BABYLON.StandardMaterial("materialMoveBlock", global.scene);
+
+        // set emissive color
+        materialMoveBlock.emissiveColor = new BABYLON.Color3(.7, .7, .7);
+        moveBlock.material = materialMoveBlock;
+
+        global.moveblock.push(moveBlock);
     });
-})();
+
+    global.scene.activeCamera.alpha = photo.camera.alpha;
+
+
+    // load material
+    var texture = new BABYLON.Texture("photos/" + current + ".jpg", global.scene);
+
+    // rotate texture
+    texture.wAng = Math.PI/-2;
+
+    // create new material
+    var materialPhotoSphere = new BABYLON.StandardMaterial("texturePhotoSphere", global.scene);
+    materialPhotoSphere.diffuseTexture = texture;
+
+    // enable backface culling
+    materialPhotoSphere.backFaceCulling = false;
+    //sphere.scaling.x = -1;
+
+    // set emissive color
+    materialPhotoSphere.emissiveColor = new BABYLON.Color3(1.0, 1.0, 1.0);
+
+    // asign material to sphere
+    sphere.material = materialPhotoSphere;
+
+}
+
+// Init babylon
+var setupBabylon = function() {
+
+    // Get the canvas element from our HTML below
+    global.canvas = document.getElementById("renderCanvas");
+
+    // Load BABYLON 3D engine
+    global.engine = new BABYLON.Engine(global.canvas, true);
+
+    // This creates a basic Babylon Scene object (non-mesh)
+    global.scene = new BABYLON.Scene(global.engine);
+
+    //global.scene.debugLayer.show();
+
+    // setup camera
+    setupNormalCamera(findPhoto("0001").camera);
+
+    // setup photosphere
+    setupPhotoSphere("0001", database);
+
+    // Once the scene is loaded, just register a render loop to render it
+    global.engine.runRenderLoop(function () {
+
+        global.scene.render();
+    });
+
+};
